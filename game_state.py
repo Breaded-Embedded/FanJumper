b3f93900e825c2c52338eb59b2442f79505782a6
Logan Furedi <kolosso@protonmail.com>
2026-03-29 16:08:14 -0500

    chore: add comments to game_state.py

--- game_state.py
+++ game_state.py
@@ -16,22 +16,29 @@ class GameState:
         self.reset()
         
     def init(self):
+        # Called when the game is opened
         pass
 
     def reset(self):
+        # Called on initialization and state changes
         pass
 
     def enter(self):
+        # Called when the state is entered
         pass
 
     def update(self):
+        # Called every frame
         pass
 
     def draw(self):
+        # Called every frame
         pass
 
     def handle_event(self, event: pygame.event):
+        # Called when a PyGame event occurs
         pass
 
     def handle_joystick_pressed(self):
+        # Called when the joystick is moved in a direction
         pass
